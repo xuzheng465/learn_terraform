096f7eb92642a3f44c7d0ecd1bc6f2749843eb8c
xuzheng465 <xuzheng465@gmail.com>
2022-08-17 14:33:19 +0800

    web server with vars

--- main.tf
+++ main.tf
@@ -16,10 +16,33 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "<h1>Better late than never</h1>" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
   }
 }
+
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 
