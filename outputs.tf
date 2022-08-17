2c0670cc62fb30e782484302d905e0e279adec6b
xuzheng465 <xuzheng465@gmail.com>
2022-08-17 16:08:04 +0800

    cluster server with load balancer

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the Instance"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
--- variables.tf
+++ variables.tf
@@ -9,3 +9,22 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
+
+
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-asg-example"
+}
+
+variable "instance_security_group_name" {
+  description = "The name of the security group for the EC2 Instances"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "terraform-example-alb"
+}
